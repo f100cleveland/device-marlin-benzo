11edf52d0ce72ed32a57fdae62ea7863020f74a6
Dustin Rinne <dustin.rinne@gmail.com>
2017-03-22 23:21:44 -0500

    benzo device bringup

---
+++ power/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c power-8996.c
+
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+ifneq ($(TARGET_USES_AOSP),true)
+    LOCAL_CFLAGS += -DEXTRA_POWERHAL_HINTS
+endif
+
+LOCAL_MODULE := power.$(patsubst %f,%,$(subst _eas,,$(subst du_,,$(TARGET_PRODUCT))))
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
